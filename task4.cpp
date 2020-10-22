db3046003c464516be61fb2adaa0e303e4e6c055
Lubo <lubohero@abv.bg>
2020-10-22 22:01:35 +0300

    added new task4

--- task4.cpp
+++ task4.cpp
@@ -2,37 +2,37 @@
 using namespace std;
 int main()
 {
-	int n = 0, N = 0, p = 1, m = 1, q = 1, x = 1, y = 1, z = 1;
-	int s=0,T=0;
+	int n = 0, N = 0, p = 0, m = 0, q = 0; //n - chisloto N koeto moje da se promenq za da ne schupi programata
+	int x = 1, y = 1, z = 1; //vremenni stoinosti za m,p,q
+	int s=0,T=0; //promenlivi s koito se sravnqva sbora
 	cin >> N;
-	n = N;
-	for (int i=1; i <= N; i++)
+	while (x <= N)
 	{
 		n = N;
-		int k = i;
-		while (k<=n)
+		if (n % x == 0)
 		{
-			if (n % k == 0)break;
-			k++;
+			n = n / x;
+			int a = 1;
+			while (a<=n)
+			{
+				y = a;
+				if (n % y == 0)
+				{
+					z = n / y;
+					s = x + y + z;
+					if (s < T || 0 == T)
+					{
+						m = x;
+						p = y;
+						q = z;
+						T = s;
+					}
+					
+				}
+				a++;
+			}
 		}
-		x = k;
-		n = n / x;
-		int b = k;
-		while (b <= n)
-		{
-			if (n % b == 0)break;
-			b++;
-		}
-		y = b;
-		z = n / b;
-		s = x + y + z;
-		if (s < T || T==0)
-		{
-			m = x;
-			p = y;
-			q = z;
-		}
-		T = s;
+		x++;
 	}
 	cout << m << " " << p << " " << q;
 	return 0;
