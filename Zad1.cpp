1b7aa07ac8e06f8b08fa88e8efe0ac6e56158d70
Lubo <lubohero@abv.bg>
2020-11-21 16:13:20 +0200

    Dobavena 1va zad

---
+++ Zad1.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+	int SpecAbs(int,int);
+	bool CheckTriangle(int, int, int, int);
+	int SizeV;
+	bool isTrue = 1;
+	do {
+		cin >> SizeV;
+	} while (SizeV < 3 || SizeV>100);
+	vector<int>seq;
+	for (int i = 0; i < SizeV; i++) {
+		int number;
+		cin >> number;
+		seq.push_back(number);
+	}
+	int N = SizeV;
+	for (int i = 0; i <= N; i++, N--) {
+		if (SizeV % 2 == 0 && i == N) break;
+		else if (SizeV % 2 != 0 && i == N - 1) break;
+		int a = seq[i], b = seq[i + 1], c = seq[N - 2], d = seq[N - 1];
+		if (CheckTriangle(a, b, c, d) == false) {
+			isTrue = 0;
+			break;
+		}
+	}
+	cout << isTrue<<endl;
+	return 0;
+}
+int SpecAbs(int a=0, int b=0) {
+	if (a - b >= 0) {
+		return a - b;
+	}
+	else {
+		return b - a;
+	}
+}
+bool CheckTriangle(int a, int b, int c, int d) {
+	int x = SpecAbs(a, b);
+	int y = SpecAbs(c, d);
+	if (x == y) return true;
+	else return false;
+}
