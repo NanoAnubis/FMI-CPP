1b754028abe0f6d9a73835fbca5c64c80770795c
Lubomir Stoykov <lubomir_stoykov@abv.bg>
2021-01-02 16:08:35 +0200

    added zad1 ot dmo 4

--- zad1.cpp
+++ zad1.cpp
@@ -2,15 +2,38 @@
 using namespace std;
 
 bool IsSequence(int* arr, int size, int number) {
-	int counter = 0;
+	int counter = 0, equal = 0;
+	bool flag = 1;
+	int* Tmp_arr = nullptr;
+	Tmp_arr = new int[size];
+	for (int i = 0; i < size; i++) {
+		Tmp_arr[i] = arr[i];
+	}
 	for (int i = 0; i < size - 1; i++) {
 		if (arr[i] > arr[i + 1]) {
 			counter++;
+			arr[i + 1] = arr[i];
 		}
 		if (counter > number) {
+			flag = 0;
+		}
+	}
+	if (flag == 1) {
+		delete[]Tmp_arr;
+		return 1;
+	}
+	counter = 0;
+	for (int i = size - 1; i > 0 ; i--) {
+		if (Tmp_arr[i] < Tmp_arr[i - 1]) {
+			counter++;
+			Tmp_arr[i - 1] = Tmp_arr[i];
+		}
+		if (counter > number) {
+			delete[]Tmp_arr;
 			return 0;
 		}
 	}
+	delete[]Tmp_arr;
 	return 1;
 }
 
