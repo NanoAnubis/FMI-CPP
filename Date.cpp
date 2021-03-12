e4b0028e9bd39ae2b8bbfef219aab753ea051576
Lubomir Stoykov <lubomir_stoykov@abv.bg>
2021-03-12 15:27:52 +0200

    added files for domashno 1 oop

---
+++ Date.cpp
@@ -0,0 +1,157 @@
+#include "Date.hpp"
+
+Date::Date()
+{
+	day = 1;
+	month = 1;
+	year = 2000;
+
+	SetCount();
+}
+
+Date::Date(int _day, int _month, int _year)
+{
+	day = _day;
+	month = _month;
+	year = _year;
+
+	SetCount();
+}
+
+void Date::addDays(int n)
+{
+	count_days += n;
+	ChangeDate();
+}
+
+void Date::removeDays(int n )
+{
+	count_days -= n;
+	ChangeDate();
+}
+
+void Date::SetCount() 
+{
+	count_days = day;
+	switch (isLeapYear())
+	{
+	case true: {
+
+		for (int i = 0; i < 12; i++) {
+			if (month == i + 1) {
+				count_days += MonthToDays_L[i];
+				break;
+			}
+		}
+
+		break;
+	}
+	case false: {
+
+		for (int i = 0; i < 12; i++) {
+			if (month == i + 1) {
+				count_days += MonthToDays_NL[i];
+				break;
+			}
+		}
+		break;
+	}
+	}
+}
+
+void Date::ChangeDate() 
+{
+	while (count_days < 0) { // If year changes to the previous one
+		year--;
+		switch (isLeapYear())
+		{
+		case true: {
+			count_days += 366;
+			break;
+		}
+		case false: {
+			count_days += 365;
+			break;
+		}
+		}
+
+	}
+	while (count_days >= 366) { // If year(L/NL) changes to the next one
+		bool flag = 0;
+		switch (isLeapYear())
+		{
+		case true: {
+			if (count_days == 366) {
+				flag = 1;
+				break;
+			}
+			year++;
+			count_days -= 366;
+			break;
+		}
+		case false: {
+			year++;
+			count_days -= 365;
+			break;
+		}
+		}
+		if (flag == 1) {
+			flag = 0;
+			break;
+		}
+	}
+
+	switch (isLeapYear())
+	{
+	case true: {
+
+		for (int i = 0; i < 12; i++) {
+			if (count_days >= MonthToDays_L[i] && count_days < MonthToDays_L[i + 1]) {
+				month = i;
+				day = count_days - MonthToDays_L[i];
+				break;
+			}
+		}
+
+		break;
+	}
+	case false: {
+
+		for (int i = 0; i < 12; i++) {
+			if (count_days >= MonthToDays_NL[i] && count_days < MonthToDays_NL[i + 1]) {
+				month = i;
+				day = count_days - MonthToDays_NL[i];
+				break;
+			}
+		}
+		break;
+	}
+	}
+}
+
+bool Date::isLeapYear()
+{
+	return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
+}
+
+int Date::DaysToXmas()
+{
+	return 0;
+}
+
+int Date::DaysToNewYear()
+{
+	return 0;
+}
+
+int Date::DaysToDate()
+{
+	return 0;
+}
+
+bool Date::isLaterThen(Date)
+{
+	return false;
+}
+
+
---
+++ Date.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+class Date
+{
+public:
+
+	Date();
+	Date(int, int, int);
+	void addDays(int);
+	void removeDays(int);
+	bool isLeapYear();
+	int DaysToXmas();
+	int DaysToNewYear();
+	int DaysToDate();
+	bool isLaterThen(Date);
+	//~Date();
+	
+
+private:
+	unsigned int day;
+	unsigned int month;
+	unsigned int year;
+	int count_days;
+
+	void SetCount();
+	void ChangeDate();
+	// 31 28 31 30 31 30 31 31 30 31 30 31 not leap year
+	// 31 29 31 30 31 30 31 31 30 31 30 31 leap year
+
+	const int MonthToDays_NL[13] = { 0,31,59,90,120,151,181,212,243,273,304,334,366 }; //last number is one higher to have the max possible number
+	const int MonthToDays_L[13] = { 0,31,60,91,121,152,182,213,244,274,305,335,367 }; //same as above
+	const int XmasDate_NL = 359;
+	const int XmasDate_L = 360;
+};
+
